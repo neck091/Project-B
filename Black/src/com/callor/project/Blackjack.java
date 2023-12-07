e79afa9a4377d7b6146181c819221ee63b9308b8
neck091 <cjstk_05@naver.com>
2023-12-07 16:45:34 +0900

    진행

--- Black/src/com/callor/project/Blackjack.java
+++ Black/src/com/callor/project/Blackjack.java
@@ -1,19 +1,31 @@
 package com.callor.project;
 
 public class Blackjack {
+
 	public static void main(String[] args) {
 
-		int[] num = new int[9];
+		int[] cards = new int[13];
+		int index = 0;
+		String card =null;
 
-		int j = 2;
-		
-		for (int i = 0; i < num.length; i++) {
-				num[i] = j;
-				j++;
-		}
-	for (int i = 0; i < num.length; i++) {
-			System.out.println(num[i]);
-		}
+		for (int j = 0; j < 4; j++) {
+			for (int i = 0; i < 13; i++) {
+				if (j == 0)
+					cards[i] = i + 1;
 
+			if (j==0) {card = String.format("◆%d", cards[i]);
+			}
+			else if (j==1) {card = String.format("♠%d", cards[i]);
+			}
+			else if (j==2) {card = String.format("♥%d", cards[i]);
+			}
+			else if (j==3) {card = String.format("♣%d", cards[i]);
+			}
+			
+			
+			System.out.println(card);
+			}
+
+		}
 	}
 }
